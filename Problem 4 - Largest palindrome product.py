742fade42175a3f5857c19f9e0d4fc8b6aeb81a9
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-01 17:19:58 -0300

    Problem 4 - Largest palindrome product - Solved

--- Problem 4 - Largest palindrome product.py
+++ Problem 4 - Largest palindrome product.py
@@ -10,3 +10,27 @@ Um número palíndromo é lido nas duas direções
 O maior palíndromo obtido com o produto de dois número de 2 dígitos é 9009 = 91 x 99
 Encontre o maior palíndromo feito a partir do produto de dois números de 3 dígitos
 """
+
+check_is_palindrome = lambda x: True if str(x)[::-1] == str(x) else False
+
+def get_largest_palin_between(x: int, y: int) -> int:
+    palindrome: int = 0
+    a = 4
+    b = 4
+    while a < x:
+        while b < y:
+            palin = a * b
+            if check_is_palindrome(palin) \
+                and len(str(a)) == 3 \
+                and len(str(b)) == 3 \
+                and palin > palindrome:
+
+                palindrome = palin
+            b += 1
+        a += 1
+        b = 4
+    return palindrome
+
+# Get the result
+answer = get_largest_palin_between(1000, 1000)
+print(answer)
