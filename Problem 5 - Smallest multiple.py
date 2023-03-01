8f998067db5e5bd96f3bed38e99dec693a731a5f
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-01 18:50:38 -0300

    Problem 5 - Smallest multiple - Start

---
+++ Problem 5 - Smallest multiple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+2520 é o menor número que pode ser dividido por cada um números entre 1 e 10 sem resto.
+Qual é o menor número positivo que é divisivel por todos os números entre 1 e 20?
+"""
