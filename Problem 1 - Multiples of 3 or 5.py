025e0f67bafb4cd994a33da2290771f60ed2682a
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-01 14:33:58 -0300

    Problem 1 - Multiples of 3 or 5 - Start

---
+++ Problem 1 - Multiples of 3 or 5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Se listarmos todos os números naturais abaixo de 10 que são multiplos de 3 OU 5, teriamos 3, 5, 6 e, 9.
+A soma desses números é 23.
+Ache a soma de todos os multiplos de 3 ou 5 abaixo de 1000.
+'''
