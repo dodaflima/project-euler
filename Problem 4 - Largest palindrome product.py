162018ba71b9f882913c24978236ec85870de9a4
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-01 17:02:44 -0300

    Problem 4 - Largest palindrome product - Start

---
+++ Problem 4 - Largest palindrome product.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+Um número palíndromo é lido nas duas direções
+O maior palíndromo obtido com o produto de dois número de 2 dígitos é 9009 = 91 x 99
+Encontre o maior palíndromo feito a partir do produto de dois números de 3 dígitos
+"""
