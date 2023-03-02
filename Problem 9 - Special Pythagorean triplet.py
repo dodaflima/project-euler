c9cee0089c866de55bf2cf9fe8ec7ee84ee7f70e
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-02 15:23:03 -0300

    Problem 9 - Special Pythagorean triplet - Start

---
+++ Problem 9 - Special Pythagorean triplet.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a² + b² = c²
+For example, 3² + 4² = 9 + 16 = 25 = 5².
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+Um terno pitagórico é um conjunto de três números naturais, a < b < c, para o qual
+a² + b² = c²
+Por exemplo, 3² + 4² = 9 + 16 = 25 = 5².
+Existe exatamente UM terno pitagórico para o qual a + b + c = 1000.
+Encontre o produto abc
+"""
+
