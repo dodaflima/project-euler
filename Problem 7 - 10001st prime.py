bf4886cc5e821bd23d4d508d21544901c6c29fe0
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-02 15:14:32 -0300

    Problem 7 - 10001st prime - Start

---
+++ Problem 7 - 10001st prime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+
+Listando os seis primeiros números primos: 2, 3, 5, 7, 11 e 13, podemos ver que o 6° número primo é o 13.
+Qual é p 10001° número primo?
+"""
