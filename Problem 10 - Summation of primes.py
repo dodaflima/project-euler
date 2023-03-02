d90e91f81a6989f3af2c4b5edac9a47747a436cb
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-02 16:49:39 -0300

    Problem 10 - Summation of primes - Start

---
+++ Problem 10 - Summation of primes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million
+
+A soma dos números primos abaixo de 10 são 2 + 3 + 5 + 7 = 17
+Encontre a soma de todos os números primos abaixo de dois milhões
+"""
