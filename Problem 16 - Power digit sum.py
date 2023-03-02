4476e3f9e37ef2d7bffdcf3853094ec0c59d85d2
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-02 17:07:28 -0300

    Problem 16 - Power digit sum - Start

---
+++ Problem 16 - Power digit sum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+2¹⁵ = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 21000?
+
+2¹⁵ = 32768 e a soma dos digitos é 3 + 2 + 7 + 6 + 8 = 26.
+Qual é a soma dos digitos do número 2¹⁰⁰⁰
+"""
