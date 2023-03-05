9f5fb2f2fc4f269403c842494f8721edc02a5f37
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-05 18:39:25 -0300

    Problem 15 - Lattice paths - Start

---
+++ Problem 15 - Lattice paths.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Starting in the top left corner of a 2x2 grid,
+and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20x20 grid?
+
+Começando do canto superior esquerdo em uma grade 2x2,
+e somente sendo capaz de se mover para direita e para baixo,
+há exatamente 6 caminhos para o canto inferior direito.
+Quantos caminhos possiveis há em uma grade 20x20?
+'''
