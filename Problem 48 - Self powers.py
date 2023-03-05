1e420c68560a2ca1d2ff6023ed169eb049ecd916
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-05 19:59:38 -0300

    Problem 48 - Self powers - Solved

--- Problem 48 - Self powers.py
+++ Problem 48 - Self powers.py
@@ -8,3 +8,8 @@ Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 10001000.
 A sequência, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
 Encontre os últimos dez digitos da sequência , 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰.
 '''
+
+answer = sum(((x+1)**(x+1)) for x in range(1000))
+
+# Answer
+print(str(answer)[:-11:-1][::-1])
