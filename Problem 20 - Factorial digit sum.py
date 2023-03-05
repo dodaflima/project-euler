221c0f374befa42ad162ed49ddf003e12404e3b5
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-05 18:55:10 -0300

    Problem 20 - Factorial digit sum - Start

---
+++ Problem 20 - Factorial digit sum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+n! means n x (n - 1) x ... x 3 x 2 x 1
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Find the sum of the digits in the number 100!
+
+n! significa n x (n - 1) x ... x 3 x 2 x 1
+Por exemplo, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+e a soma dos digitos do número é 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Encontre a soma dos digitos do número 100!
+'''
