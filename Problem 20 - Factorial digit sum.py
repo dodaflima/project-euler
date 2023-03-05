4d843934af697aa4fa5cea2b3f692559c77e2987
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-05 18:55:32 -0300

    Problem 20 - Factorial digit sum - Solved

--- Problem 20 - Factorial digit sum.py
+++ Problem 20 - Factorial digit sum.py
@@ -12,3 +12,18 @@ Por exemplo, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
 e a soma dos digitos do número é 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Encontre a soma dos digitos do número 100!
 '''
+
+from math import prod
+
+def fat(number):
+    number = abs(number)
+    numbers = []
+    while number > 0:
+        numbers.append(number)
+        number -= 1
+    return prod(numbers)
+
+digits_sum = lambda number: sum([int(x) for x in str(fat(number))])
+
+# Answer
+print(digits_sum(100))
