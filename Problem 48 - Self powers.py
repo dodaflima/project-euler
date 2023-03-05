c84556248aa137e49ec254b9221597bcdba91801
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-05 19:59:01 -0300

    Problem 48 - Self powers - Start

---
+++ Problem 48 - Self powers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+The series, 1¹ + 2² + 3³ + ... + 10¹⁰= 10405071317.
+Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 10001000.
+
+A sequência, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
+Encontre os últimos dez digitos da sequência , 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰.
+'''
