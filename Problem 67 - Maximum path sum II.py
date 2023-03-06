27b738be7121ce5c7b9bea6b03464810ff118ceb
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-06 08:52:46 -0300

    Problem 67 - Maximum path sum II - Start

---
+++ Problem 67 - Maximum path sum II.py
@@ -0,0 +1,30 @@
+'''
+By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+the maximum total from top to bottom is 23.
+   3
+  7 4
+ 2 4 6
+8 5 9 3
+That is, 3 + 7 + 4 + 9 = 23.
+Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'),
+a 15K text file containing a triangle with one-hundred rows.
+NOTE: This is a much more difficult version of Problem 18.
+It is not possible to try every route to solve this problem, as there are 2⁹⁹ altogether!
+If you could check one trillion (10¹²) routes every second it would take over
+twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
+
+Começando do topo do triângulo abaixo e movendo para os números adjacente na linha de baixo,
+o máximo total do topo para o final é 23
+   3
+  7 4
+ 2 4 6
+8 5 9 3
+Isto é..., 3 + 7 + 4 + 9 = 23.
+
+ENcontre o máximo total do topo para a base em [https://projecteuler.net/project/resources/p067_triangle.txt],
+Um arquivo de texto de 15K contendo um triângulo com cem linhas.
+NOTA: Este é uma versão muito mais difícil do Problema 18.
+Não é possível tentar cada rota para resolver este problema, pois há 2⁹⁹ no total!
+Se você pudesse checar um trilhão (10¹²) de rotas a cada segundo, poderia levar 20 bilhões de anos
+para checar todas elas. Existe um algoritmo eficiente para resolvê-lo. ;o)
+'''
