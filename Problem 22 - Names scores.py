7f8ffc2f517d1fec4bdfa41b59cc50b6cf50ba7e
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-06 09:51:10 -0300

    Problem 22 - Names scores - Start

---
+++ Problem 22 - Names scores.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Using names.txt (right click and 'Save Link/Target As...'),
+a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order. Then working out the alphabetical value for each name,
+multiply this value by its alphabetical position in the list to obtain a name score.
+For example, when the list is sorted into alphabetical order, COLIN,
+which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 x 53 = 49714.
+What is the total of all the name scores in the file?
+
+Usando https://projecteuler.net/project/resources/p022_names.txt
+Um arquivo de texto co 45K contendo mais de 5000 nomes, 
+Sendo colocados em ordem alfabética. E então, calculando o valor alfabético para cada nome,
+multiplicando esse valor pela posição na lista para obter a pontuação do nome.
+Por exemplo, quando a lista e ordenada em ordem alfabética, COLIN, vale 3 + 15 + 12 + 9 + 14 = 53,
+está na 938° posição na lista.
+Então, COLIN tem uma pontuação de 938 x 53 = 49714.
+Qual é a soma da pontuação de todos os nomes no arquivo?
+'''
