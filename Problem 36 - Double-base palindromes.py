d2c94a057df8f9b69b9ab79d4fc88cc771965a73
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-03-06 12:53:01 -0300

    Problem 36 - Double-base palindromes - Solved

--- Problem 36 - Double-base palindromes.py
+++ Problem 36 - Double-base palindromes.py
@@ -8,3 +8,14 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 (Please note that the palindromic number, in either base, may not include leading zeros.)
 """
+
+is_palindrome = lambda x: x == x[::-1]
+to_bin_number = lambda x: str(bin(x))[2:]
+numbers_sum = 0
+
+for i in range(1, 1000001):
+    if is_palindrome(str(i)) and is_palindrome(to_bin_number(i)):
+        numbers_sum += i
+
+# Answer
+print(numbers_sum)
